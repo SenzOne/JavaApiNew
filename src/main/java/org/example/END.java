db710798292765481a654d21bc309298c259b862
Senz One <sem63sam@yandex.ru>
2023-10-15 04:59:03 +0400

    add task 1 new s

---
+++ src/main/java/org/example/END.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class END {
+    public static void main(String[] args) {
+        System.out.println("ldskfjsldfkj");
+        System.out.println("ldskfjsldfkj");
+        System.out.println("ldskfjsldfkj");
+        System.out.println("ldskfjsldfkj");
+        System.out.println("ldskfjsldfkj");
+        System.out.println("ldskfjsldfkj");
+        System.out.println("ldskfjsldfkj");
+    }
+}
