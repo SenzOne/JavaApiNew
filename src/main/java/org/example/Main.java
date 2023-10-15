8dc84ac5de42feb65a2cb4f2facd43fe78c1e26a
Senz One <sem63sam@yandex.ru>
2023-10-15 04:11:12 +0400

    add task 1 new s

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+
+
+        System.out.println("hhahahahah");
     }
 }
