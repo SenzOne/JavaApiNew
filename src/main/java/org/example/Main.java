4d84e6d5d6e19a2a965e48c39e38d02b344200ce
Senz One <sem63sam@yandex.ru>
2023-10-15 04:55:17 +0400

    add task 1 new s

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,20 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
-
-        System.out.println("hhahahahah");
-        System.out.println("hhahahahah");
-        System.out.println("hhahahahah");
-        System.out.println("hhahahahah");
-        System.out.println("hhahahahah");
-        System.out.println("hhahahahah");
+        System.out.println("sldfkjsdlfkj");
+        System.out.println("sldfkjsdlfkj");
+        System.out.println("sldfkjsdlfkj");
+        System.out.println("sldfkjsdlfkj");
+        System.out.println("sldfkjsdlfkj");
+        System.out.println("sldfkjsdlfkj");
+        System.out.println("sldfkjsdlfkj");
+        System.out.println("sldfkjsdlfkj");
     }
 }
