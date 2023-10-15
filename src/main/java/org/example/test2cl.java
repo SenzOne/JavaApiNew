62791cabbfcf9ab3935705209dc90b8056a62f4b
Senz One <sem63sam@yandex.ru>
2023-10-15 04:12:37 +0400

    add task 1 new s

---
+++ src/main/java/org/example/test2cl.java
@@ -0,0 +1,1 @@
+package org.example;public class test2cl {    public static void main(String[] args) {        System.out.println("sdlfjsldfkjsldfkj");        System.out.println("sdlfjsldfkjsldfkj");        System.out.println("sdlfjsldfkjsldfkj");        System.out.println("sdlfjsldfkjsldfkj");        System.out.println("sldfkjslfk");    }}
